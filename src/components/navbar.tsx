d33e17b070c4e20c0606b41c327afdb0bf114766
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-15 19:38:08 +0100

    🔒️ added admin role support

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,299 +1,331 @@
-'use client';
+"use client";
 import {
-    Box,
-    Flex,
-    Text,
-    IconButton,
-    Button,
-    Stack,
-    Collapse,
-    Icon,
-    Link,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    useColorMode,
-    useColorModeValue,
-    useBreakpointValue,
-    useDisclosure,
-} from '@chakra-ui/react';
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorMode,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import {
-    HamburgerIcon,
-    CloseIcon,
-    ChevronDownIcon,
-    ChevronRightIcon,
-    MoonIcon,
-    SunIcon,
-} from '@chakra-ui/icons';
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+  MoonIcon,
+  SunIcon,
+} from "@chakra-ui/icons";
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 import React from "react";
-import {signOut} from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function WithSubnavigation() {
-    const {isOpen, onToggle} = useDisclosure();
-    const {colorMode, toggleColorMode} = useColorMode();
-    return (
-        <Box>
-            <Flex
-                bg={useColorModeValue('white', 'gray.800')}
-                color={useColorModeValue('gray.600', 'white')}
-                minH={'60px'}
-                py={{base: 2}}
-                px={{base: 4}}
-                borderBottom={1}
-                borderStyle={'solid'}
-                borderColor={useColorModeValue('gray.200', 'gray.900')}
-                align={'center'}>
-                <Flex
-                    flex={{base: 1, md: 'auto'}}
-                    ml={{base: -2}}
-                    display={{base: 'flex', md: 'none'}}>
+  const { isOpen, onToggle } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { data: session } = useSession({ required: true });
 
-                    <IconButton
-                        onClick={onToggle}
-                        icon={
-                            isOpen ? <CloseIcon w={3} h={3}/> : <HamburgerIcon w={5} h={5}/>
-                        }
-                        variant={'ghost'}
-                        aria-label={'Toggle Navigation'}
-                    />
+  const isAdmin = session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL;
 
-                </Flex>
-                <Flex flex={{base: 1}} justify={{base: 'center', md: 'start'}}>
-                    <Text
-                        textAlign={useBreakpointValue({base: 'center', md: 'left'})}
-                        fontFamily={'heading'}
-                        color={useColorModeValue('gray.800', 'white')}>
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Flex
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Text
+            textAlign={useBreakpointValue({ base: "center", md: "left" })}
+            fontFamily={"heading"}
+            color={useColorModeValue("gray.800", "white")}
+          >
+            <Link href={"/"}>
+              <Image
+                className={styles.logo}
+                src="/you-pod-nav.svg"
+                alt="You-Pod Logo"
+                width={100}
+                height={37}
+                priority
+              />
+            </Link>
+          </Text>
 
-                        <Link href={'/'}>
-                            <Image
-                                className={styles.logo}
-                                src="/you-pod-nav.svg"
-                                alt="You-Pod Logo"
-                                width={100}
-                                height={37}
-                                priority
-                            />
-                        </Link>
-                    </Text>
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <DesktopNav isAdmin={isAdmin} />
+          </Flex>
+        </Flex>
 
-                    <Flex display={{base: 'none', md: 'flex'}} ml={10}>
-                        <DesktopNav/>
-                    </Flex>
-                </Flex>
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
 
-                <Stack
-                    flex={{base: 1, md: 0}}
-                    justify={'flex-end'}
-                    direction={'row'}
-                    spacing={6}>
-                    <Button onClick={toggleColorMode}>
-                        {colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}
-                    </Button>
+          <Button
+            minW={"24"}
+            onClick={() => signOut()}
+            as={"a"}
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bg={"gray.900"}
+            href={"/"}
+            _hover={{
+              bg: "gray.700",
+            }}
+          >
+            Sign Out
+          </Button>
 
-                    <Button
-                        minW={"24"}
-                        onClick={() => signOut()}
-                        as={'a'}
-                        display={{base: 'none', md: 'inline-flex'}}
-                        fontSize={'sm'}
-                        fontWeight={600}
-                        color={'white'}
-                        bg={'gray.900'}
-                        href={'/'}
-                        _hover={{
-                            bg: 'gray.700',
-                        }}>
-                        Sign Out
-                    </Button>
-
-                    {/*<Button*/}
-                    {/*    minW={"24"}*/}
-                    {/*    as={'a'}*/}
-                    {/*    display={{base: 'none', md: 'inline-flex'}}*/}
-                    {/*    fontSize={'sm'}*/}
-                    {/*    fontWeight={600}*/}
-                    {/*    color={'white'}*/}
-                    {/*    bg={'gray.900'}*/}
-                    {/*    href={'sign_up'}*/}
-                    {/*    _hover={{*/}
-                    {/*        bg: 'gray.700',*/}
-                    {/*    }}>*/}
-                    {/*    Sign Up*/}
-                    {/*</Button>*/}
-                </Stack>
-            </Flex>
-            <Collapse in={isOpen} animateOpacity>
-                <MobileNav/>
-            </Collapse>
-        </Box>
-    );
+          {/*<Button*/}
+          {/*    minW={"24"}*/}
+          {/*    as={'a'}*/}
+          {/*    display={{base: 'none', md: 'inline-flex'}}*/}
+          {/*    fontSize={'sm'}*/}
+          {/*    fontWeight={600}*/}
+          {/*    color={'white'}*/}
+          {/*    bg={'gray.900'}*/}
+          {/*    href={'sign_up'}*/}
+          {/*    _hover={{*/}
+          {/*        bg: 'gray.700',*/}
+          {/*    }}>*/}
+          {/*    Sign Up*/}
+          {/*</Button>*/}
+        </Stack>
+      </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav isAdmin={isAdmin}/>
+      </Collapse>
+    </Box>
+  );
 }
 
-const DesktopNav = () => {
-    const linkColor = useColorModeValue('gray.600', 'gray.200');
-    const linkHoverColor = useColorModeValue('gray.800', 'white');
-    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-    return (
-        <Stack direction={'row'} spacing={4}>
-            {NAV_ITEMS.map((navItem) => (
-                <Box key={navItem.label}>
-                    <Popover trigger={'hover'} placement={'bottom-start'}>
+interface DesktopNavProps {
+  isAdmin: boolean;
+}
 
-                        <PopoverTrigger>
-                            <Link
-                                p={2}
-                                href={navItem.href ?? '#'}
-                                fontSize={'sm'}
-                                fontWeight={500}
-                                color={linkColor}
-                                _hover={{
-                                    textDecoration: 'none',
-                                    color: linkHoverColor,
-                                }}>
-                                {navItem.label}
-                            </Link>
-                        </PopoverTrigger>
-
-                        {navItem.children && (
-                            <PopoverContent
-                                border={0}
-                                boxShadow={'xl'}
-                                bg={popoverContentBgColor}
-                                p={4}
-                                rounded={'xl'}
-                                minW={'sm'}>
-                                <Stack>
-                                    {navItem.children.map((child) => (
-                                        <DesktopSubNav key={child.label} {...child} />
-                                    ))}
-                                </Stack>
-                            </PopoverContent>
-                        )}
-                    </Popover>
-                </Box>
-            ))}
-        </Stack>
-    );
-};
-
-const DesktopSubNav = ({label, href, subLabel}: NavItem) => {
-    return (
-        <Link
-            href={href}
-            role={'group'}
-            display={'block'}
-            p={2}
-            rounded={'md'}
-            _hover={{bg: useColorModeValue('pink.50', 'gray.900')}}>
-            <Stack direction={'row'} align={'center'}>
-                <Box>
-                    <Text
-                        transition={'all .3s ease'}
-                        _groupHover={{color: 'pink.400'}}
-                        fontWeight={500}>
-                        {label}
-                    </Text>
-                    <Text fontSize={'sm'}>{subLabel}</Text>
-                </Box>
-                <Flex
-                    transition={'all .3s ease'}
-                    transform={'translateX(-10px)'}
-                    opacity={0}
-                    _groupHover={{opacity: '100%', transform: 'translateX(0)'}}
-                    justify={'flex-end'}
-                    align={'center'}
-                    flex={1}>
-                    <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon}/>
-                </Flex>
-            </Stack>
-        </Link>
-    );
-};
-
-const MobileNav = () => {
-    return (
-        <Stack
-            bg={useColorModeValue('white', 'gray.800')}
-            p={4}
-            display={{md: 'none'}}>
-            {NAV_ITEMS.map((navItem) => (
-                <MobileNavItem key={navItem.label} {...navItem} />
-            ))}
-        </Stack>
-    );
-};
-
-const MobileNavItem = ({label, children, href}: NavItem) => {
-    const {isOpen, onToggle} = useDisclosure();
-
-    return (
-        <Stack spacing={4} onClick={children && onToggle}>
-            <Flex
-                py={2}
-                as={Link}
-                href={href ?? '#'}
-                justify={'space-between'}
-                align={'center'}
+const DesktopNav = ({ isAdmin }: DesktopNavProps) => {
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+  const navElements = isAdmin
+    ? NAV_ITEMS
+    : NAV_ITEMS.filter((navItem) => navItem.label !== "Admin");
+  return (
+    <Stack direction={"row"} spacing={4}>
+      {navElements.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? "#"}
+                fontSize={"sm"}
+                fontWeight={500}
+                color={linkColor}
                 _hover={{
-                    textDecoration: 'none',
-                }}>
-                <Text
-                    fontWeight={600}
-                    color={useColorModeValue('gray.600', 'gray.200')}>
-                    {label}
-                </Text>
-                {children && (
-                    <Icon
-                        as={ChevronDownIcon}
-                        transition={'all .25s ease-in-out'}
-                        transform={isOpen ? 'rotate(180deg)' : ''}
-                        w={6}
-                        h={6}
-                    />
-                )}
-            </Flex>
+                  textDecoration: "none",
+                  color: linkHoverColor,
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
 
-            <Collapse in={isOpen} animateOpacity style={{marginTop: '0!important'}}>
-                <Stack
-                    mt={2}
-                    pl={4}
-                    borderLeft={1}
-                    borderStyle={'solid'}
-                    borderColor={useColorModeValue('gray.200', 'gray.700')}
-                    align={'start'}>
-                    {children &&
-                        children.map((child) => (
-                            <Link key={child.label} py={2} href={child.href}>
-                                {child.label}
-                            </Link>
-                        ))}
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} navItem={child} />
+                  ))}
                 </Stack>
-            </Collapse>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+interface DesktopSubNavProps {
+  navItem: NavItem;
+}
+
+const DesktopSubNav = ({ navItem }: DesktopSubNavProps) => {
+  return (
+    <Link
+      href={navItem.href}
+      role={"group"}
+      display={"block"}
+      p={2}
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+    >
+      <Stack direction={"row"} align={"center"}>
+        <Box>
+          <Text
+            transition={"all .3s ease"}
+            _groupHover={{ color: "pink.400" }}
+            fontWeight={500}
+          >
+            {navItem.label}
+          </Text>
+          <Text fontSize={"sm"}>{navItem.subLabel}</Text>
+        </Box>
+        <Flex
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
+          opacity={0}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
+          flex={1}
+        >
+          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+interface MobileNavProps {
+  isAdmin: boolean;
+}
+
+const MobileNav = (isAdmin: MobileNavProps) => {
+  const navElements = isAdmin
+    ? NAV_ITEMS
+    : NAV_ITEMS.filter((navItem) => navItem.label !== "Admin");
+
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {navElements.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? "#"}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
         </Stack>
-    );
+      </Collapse>
+    </Stack>
+  );
 };
 
 interface NavItem {
-    label: string;
-    subLabel?: string;
-    children?: Array<NavItem>;
-    href?: string;
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-
-    {
-        label: 'Podcast',
-        href: '/podcast',
-    },
-    {
-        label: 'Newsletter',
-        href: '/newsletter',
-    },
-    {
-        label: 'Admin',
-        href: '/admin',
-    },
+  {
+    label: "Podcast",
+    href: "/podcast",
+  },
+  {
+    label: "Newsletter",
+    href: "/newsletter",
+  },
+  {
+    label: "Admin",
+    href: "/admin",
+  },
 ];
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { useSession } from "next-auth/react";
 import Podcast from "@/components/admin_render";
 import Layout from "@/components/layout";
 import {
   Button,
   Flex,
-  Spinner,
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
@@ -18,33 +17,10 @@ export default function PodcastRender() {
     session?.user?.email === process.env.NEXT_PUBLIC_ADMIN_EMAIL
   ) {
     return (
-      <Suspense
-        fallback={
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
-          />
-        }
-      >
         <Podcast />
-      </Suspense>
     );
   }
   return (
-    <Suspense
-      fallback={
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      }
-    >
       <Layout props={""}>
         <Flex
           minH={"100vh"}
@@ -57,6 +33,7 @@ export default function PodcastRender() {
           <Text fontSize={"xl"}>❌ Acceso denegado</Text>
 
           <Button
+            // eslint-disable-next-line react-hooks/rules-of-hooks
             bg={useColorModeValue("purple.400", "purple.300")}
             _hover={{
               bg: "purple.500",
@@ -67,6 +44,5 @@ export default function PodcastRender() {
           </Button>
         </Flex>
       </Layout>
-    </Suspense>
   );
 }
