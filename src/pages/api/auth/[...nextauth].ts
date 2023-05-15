15826e46a25f55d2b8bbca9558758aaf0c782d0e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-15 16:35:29 +0100

    🐛 fix type based on undefined

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,9 @@ export default NextAuth({
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            privateKey: process.env.FIREBASE_PRIVATE_KEY !== undefined
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            : '',
         }),
     }),
     providers: [
